2ec02d8166b2ba5c3ee825bd840ff24a82029f47
Genty Julien <M.G.dev.management@gmail.com>
2023-09-09 16:27:37 +0200

    Configuration de lancement en accord avec la maquette. (VUE: ajout des deux composants principaux. SASS: ajout des variables et des mixins. HTML: ajout des polices d'écriture et de l'icône de l'onglet)

---
+++ src/components/FormContact.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ name }}</h2>
+</template>
+
+<script setup lang="ts">
+const name = "FormContact"
+</script>
+
+<style scoped lang="scss">
+@import "../assets/main.scss";
+
+h2{
+    width: 50%; height: 100vh;
+    @include font-title-bold--secondary;
+    background-color: map-get($colour, "primary");
+}
+</style>
