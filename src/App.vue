8698111fb3108dbd17d0b3302bfa354374675c58
Genty Julien <M.G.dev.management@gmail.com>
2023-09-14 16:30:34 +0200

    Mise à jour du projet, pas de 3D, 100%CSS pour s'entraîner dessus

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <div></div>
+  <div>
+  </div>
 </template>
 
 <script setup lang="ts">
+
 </script>
 
-<style scoped lang="scss">
-@import "./assets/main.scss";
+<style scoped>
 </style>
